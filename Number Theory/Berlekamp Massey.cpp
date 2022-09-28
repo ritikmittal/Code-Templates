df33b642bf576ab1d37aed51fb46812866a8a7a9
rishab-nahar <63296530+rishab-nahar@users.noreply.github.com>
2022-09-28 15:09:55 +0530

    Update Berlekamp Massey.cpp

--- Number Theory/Berlekamp Massey.cpp
+++ Number Theory/Berlekamp Massey.cpp
@@ -23,3 +23,17 @@ vector<ll> berlekampMassey(vector<ll> s) {
 }
 //{0, 1, 1, 2, 3, 5} {1, 1}, {0, 1, 1, 1, 0, -1} {1, 0, 1000000006}
 //{0, 1, 423, 43, 0, 543} {402187050, 874879090, 632104496}
+
+typedef vector<double> vd;
+vd interpolate(vd x, vd y, int n) {
+    vd res(n), temp(n);
+    rep(k,0,n-1) rep(i,k+1,n)
+    y[i] = (y[i] - y[k]) / (x[i] - x[k]);
+    double last = 0; temp[0] = 1;
+    rep(k,0,n) rep(i,0,n) {
+        res[i] += y[k] * temp[i];
+        swap(last, temp[i]);
+        temp[i] -= last * x[k];
+    }
+    return res;
+}
