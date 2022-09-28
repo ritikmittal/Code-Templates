1a9b612a403885f84036195e0473e676504a5a0e
rishab-nahar <63296530+rishab-nahar@users.noreply.github.com>
2022-09-28 11:02:52 +0530

    final wala

--- Tricks/Hacks.cpp
+++ Tricks/Hacks.cpp
@@ -6,38 +6,29 @@ ll ternSearch(ll a, ll b) {
     while (b - a >= 5) {
         ll mid = (a + b) / 2;
         if (f(mid) < f(mid+1)) a = mid; // (A)
-        else b = mid+1;
-    }
+        else b = mid+1;}
     for(ll i=a+1;i<(b+1),i++) if (f(a) < f(i)) a = i; // (B)
     return a;
 }
 
 // checks if a*b will overflow
 bool is(ll a,ll b){
-    if(a==0 || b==0)
-        return false;
+    if(a==0 || b==0) return false;
     if((a>LLONG_MAX/b) or ( b>LLONG_MAX/a)) return true;
     return false;
 }
-
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
         x += 0x9e3779b97f4a7c15;
         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
+        return x ^ (x >> 31);}
     size_t operator()(uint64_t x) const {
         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-
+        return splitmix64(x + FIXED_RANDOM);}
     size_t operator()(pair<uint64_t,uint64_t> x) const {
         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);
-    }
+        return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}
 
     size_t operator()(vector<ll> x) const {
         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
@@ -50,12 +41,8 @@ struct custom_hash {
 };
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-//ll val=rng();
-//vll v;
-//shuffle(all(v),rng);
-ll rand(ll x,ll y){
-    return uniform_int_distribution<ll>(x,y)(rng);
-}
+//ll val=rng();vll v;shuffle(all(v),rng);
+ll rand(ll x,ll y){return uniform_int_distribution<ll>(x,y)(rng);}
 
 std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 start = std::chrono::high_resolution_clock::now();
