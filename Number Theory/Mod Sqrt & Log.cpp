6367482ec6d5795da22c020677eaf2b54fb21fca
rishab-nahar <63296530+rishab-nahar@users.noreply.github.com>
2022-09-28 15:07:41 +0530

    Update Mod Sqrt & Log.cpp

--- Number Theory/Mod Sqrt & Log.cpp
+++ Number Theory/Mod Sqrt & Log.cpp
@@ -42,3 +42,17 @@ int modLog(int a, int b, int m) {
 }
 // (modLog(2,15,29)): 27
 // (modLog(3,15,29)): 11
+
+//sums of mod’ed arithmetic progressions.
+ull sumsq(ull to) { return to / 2 * ((to-1) | 1); }
+ull divsum(ull to, ull c, ull k, ull m) {
+ull res = k / m * sumsq(to) + c / m * to;
+k %= m; c %= m;
+if (!k) return res;
+ull to2 = (to * k + c) / m;
+return res + (to - 1) * to2 - divsum(to2, m-1 - c, m, k);}
+ll modsum(ull to, ll c, ll k, ll m) {
+c = ((c % m) + m) % m;
+k = ((k % m) + m) % m;
+return to * c + k * sumsq(to) - m * divsum(to, c, k, m);
+}
