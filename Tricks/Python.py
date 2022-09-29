b8de8322f10dca36c97322bdbd6d0e13e1f970e6
rishab-nahar <63296530+rishab-nahar@users.noreply.github.com>
2022-09-29 09:07:41 +0530

    Create Python.py

---
+++ Tricks/Python.py
@@ -0,0 +1,7 @@
+import sys
+from bisect import bisect
+from collections import deque, defaultdict, Counter
+from itertools import permutations as permutate
+from itertools import combinations
+def inp(): return sys.stdin.readline().rstrip("\r\n")  # for fast input
+def out(var): sys.stdout.write(str(var))  # for fast output, always take string
