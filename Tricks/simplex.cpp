419e5c6ddeaa3805c4826fbb3a1d525426803491
Ritik Mittal <55192608+ritikmittal@users.noreply.github.com>
2022-10-03 10:04:02 +0530

    msg

--- Tricks/simplex.cpp
+++ Tricks/simplex.cpp
@@ -5,14 +5,14 @@ const T eps = 1e-8, inf = 1/.0;
 #define ltj(X) if(s == -1 || MP(X[j],N[j]) < MP(X[s],N[s])) s=j
 struct LPSolver { int m, n; vi N,B; vvd D;
 LPSolver(const vvd&A,const vd&b,const vd&c):
- m(sz(b)),n(sz(c)),N(n+1),B(m),D(m+2,vd(n+2)){
+m(sz(b)),n(sz(c)),N(n+1),B(m),D(m+2,vd(n+2)){
  rep(i,0,m) rep(j,0,n) D[i][j] = A[i][j];
-rep(i,0,m){B[i]=n+i;D[i][n]=-1;D[i][n+1]=b[i]}
- rep(j,0,n) { N[j] = j; D[m][j] = -c[j]; }
+rep(i,0,m){B[i]=n+i;D[i][n]=-1;D[i][n+1]=b[i]
+;} rep(j,0,n) { N[j] = j; D[m][j] = -c[j]; }
  N[n] = -1; D[m+1][n] = 1; }
 void pivot(int r, int s) {
  T *a = D[r].data(), inv = 1 / a[s];
- rep(i,0,m+2) if (i != r && abs(D[i][s])>eps){
+ rep(i,0,m+2) if(i != r && abs(D[i][s])>eps){
      T *b = D[i].data(), inv2 = b[s] * inv;
  rep(j,0,n+2) b[j] -= a[j] * inv2;
      b[s] = a[s] * inv2; }
@@ -30,11 +30,11 @@ bool simplex(int phase) {
          <MP(D[r][n+1]/D[r][s],B[r]))r = i; }
   if (r == -1) return false; pivot(r, s); } }
 T solve(vd &x) {
- int r=0;rep(i,1,m)if(D[i][n+1]<D[r][n+1])r=i;
+int r=0;rep(i,1,m)if(D[i][n+1]<D[r][n+1])r=i;
  if (D[r][n+1] < -eps) { pivot(r, n);
-  if(!simplex(2)||D[m+1][n+1]<-eps)return-inf;
+ if(!simplex(2)||D[m+1][n+1]<-eps)return-inf;
   rep(i,0,m) if (B[i] == -1) { int s = 0;
   rep(j,1,n+1) ltj(D[i]); pivot(i, s); } }
   bool ok = simplex(1); x = vd(n);
-  rep(i,0,m) if (B[i] < n) x[B[i]] = D[i][n+1];
+  rep(i,0,m) if (B[i] < n) x[B[i]]=D[i][n+1];
   return ok ? D[m][n+1] : inf; }};
